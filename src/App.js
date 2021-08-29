2cd1f02be6ea17647d6555eb9b1c9404d4daab19
Dung Lu <dung.lu@kyanon.digital>
2021-08-29 23:03:51 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Provider } from "react-redux"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import "./App.css"
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
 import store from "./redux/Store"
 import Dashboard from "./views/dashboard"
 
@@ -8,11 +10,17 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-        </Switch>
+        <div class="d-flex" id="wrapper">
+          <Sidebar></Sidebar>
+          <div id="page-content-wrapper">
+            <Header></Header>
+            <Switch>
+              <Route exact path="/">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </Router>
     </Provider>
   )
